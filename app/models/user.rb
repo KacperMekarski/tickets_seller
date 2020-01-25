6a42d9c2f73a7dbf6552c3175c565f2ab70ccbf8
Kacper Mękarski <kacper.mekarski@gmail.com>
2020-01-25 12:20:26 +0100

    Configured RSpec, created User entity

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
+  validates_uniqueness_of :email
+  validates :full_name, presence: true
+
+  # has_many :payments
+  # has_many :tickets, through: :payments
+end
---
+++ spec/app/models/user_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'validations' do
+    subject(:user) { create(:user) }
+
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_uniqueness_of(:email) }
+    it { is_expected.to allow_value('example@email.com').for(:email) }
+    it { is_expected.not_to allow_value('example@email').for(:email) }
+    it { is_expected.not_to allow_value('example$email.com').for(:email) }
+
+    it { is_expected.to validate_presence_of(:full_name) }
+  end
+
+  # describe 'relations' do
+  #   it { is_expected.to have_many(:payments) }
+  #   it { is_expected.to have_many(:tickets).through(:payments) }
+  # end
+end
